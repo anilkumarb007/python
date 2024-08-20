ae61348171faeff1e5344e0e84857ecda015793d
anilkumarb007 <anilkumarborra007@gmail.com>
2024-08-20 08:05:30 +0530

    update with other statements

--- main.py
+++ main.py
@@ -21,8 +21,43 @@ else:
 """for value in range(1, 10, 2):
     print(value+1)
 """
-attendance = [18,12,15,5,20,10,87,12]
+"""attendance = [18,12,15,5,20,10,87,12]
 day = 1
 for element in attendance:
     print("Day ",day, 'attendance count is :',element)
     day = day + 1
+"""
+
+"""
+value = 1
+status = True 
+while status:
+    print(value) 
+    if value>5:
+        break
+    value+=1 
+    """
+    
+'''#continue statement
+incrementTill = 20
+incrementBy = 1
+forTable = int(input("Enter table number"))
+while incrementBy <= incrementTill:
+    print()
+    if incrementBy == 5:
+        incrementBy = incrementBy + 1
+        continue
+    print(forTable,' * ',incrementBy,' = ',(forTable * incrementBy))
+    incrementBy = incrementBy + 1'''
+    
+incrementTill = 20
+incrementBy = 1
+forTable = int(input("Enter table number"))
+while incrementBy <= incrementTill:
+    if incrementBy == 5:
+        pass    
+    else:
+        print("incrementBy value is :", incrementBy)
+    incrementBy = incrementBy+1
+    
+
